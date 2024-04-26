14253a2f0eab3df1f4ed21f65f09aa9651e56683
Anzu986 <den.ovsyankin@gmail.com>
2024-04-26 18:30:15 +0300

    добавили заготовки для удаления игры по id и добавления новой

--- routes/games.js
+++ routes/games.js
@@ -1,7 +1,13 @@
 const gameRouter = require ('express').Router();
 
 const getAllGames = async (req, res) => {};
+const deleteGame = async (req, res) => {};
+const addeGame = async (req, res) => {};
 
 gameRouter.get ('/games', getAllGames);
+gameRouter.delete('/games/:id', deleteGame);
+gameRouter.post('/games/:id', addeGame);
 
- module.exports = gameRouter;
+
+
+module.exports = gameRouter;
