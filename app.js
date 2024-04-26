7103fca2359e3d4b0a887c2a89c1b063ff25e1d5
Anzu986 <den.ovsyankin@gmail.com>
2024-04-26 18:15:20 +0300

    добавили ahjyntyl d gf,kbr

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require ('express')
 const app = express();
-
+const fs = require('fs').promises;
+const path = require ('path')
 const PORT = 3000;
 
-app.get('/', (req, res) =>[
-res.send('hello')
-])
+app.get('/', (req, res) =>{
+fs.readFile('./public/index.html', 'utf-8').then((data) =>{
+    res.header('Content-type' , 'text/html');
+    res.send(data);
+})
+});
+app.use (express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () =>{
     console.log(`приложение запущено тут : http://localhost:${PORT}`);
