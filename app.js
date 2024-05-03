9a460789e61d1cd38dc3570073c0e5e13981e040
Anzu986 <den.ovsyankin@gmail.com>
2024-05-03 20:05:31 +0300

    довили cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const path = require ('path');
 const mainRouter = require('./routes/main');
 const gameRouter = require('./routes/games');
+const { cors } = require('./middlewares/cors');
 
 
 const PORT = 3000;
@@ -12,6 +13,7 @@ const PORT = 3000;
 
 
 app.use (
+    cors,
     bodyParser.json(),
     express.static(path.join(__dirname, 'public')),
 mainRouter,
